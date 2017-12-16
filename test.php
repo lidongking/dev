947fdd9e45331e9eb70d1d8600d8b45f6efbe71c
Jelly <lidongking@126.com>
2017-12-16 15:19:31 +0800

    m

--- test.php
+++ test.php
@@ -13,11 +13,11 @@ include_once dirname(__FILE__) . '/inc/config.php';
 include_once dirname(__FILE__) . '/libs/xiaotu/autoload.php';
 
 $db = \Xiaotu\DataBase\MySQL::getInstance();
-$res = $db->table('test')->select()->where('id=:id', array('id' => 1))->find();
+$res = $db->table('test')->select()->where('id=:id', array('id' => 2))->find();
 var_dump($res);
 
 $db->table('test')->update(array('title' => 'new title updated'))->where('title=:title', array('title' => 'test'))->execute();
-$db->table('test')->delete()->where('id=:id', array('id' => 1))->execute();
+//$db->table('test')->delete()->where('id=:id', array('id' => 1))->execute();
 $db->table('test')->insert(array('title' => 'this is title', 'content' => 'this is content'))->execute();
 
 $data = $db->table('test')->select()->findAll();
